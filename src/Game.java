a6b82f0382d4315b8b04360f2b38bc5315af3bac
soapsudo <569681@student.saxion.nl>
2024-11-20 15:46:50 +0100

    [Game] Added the restart and back to main buttons.

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,3 @@
-import Grid.Grid;
 import utils.*;
 import nl.saxion.app.interaction.KeyboardEvent;
 import nl.saxion.app.interaction.MouseEvent;
@@ -10,8 +9,20 @@ public class Game extends Canvas{
     }
 
 
+    utils.MyButton restartButton = new MyButton();
+    utils.MyButton quitButton = new MyButton();
+
     @Override
     public void init() {
+        restartButton.x = Settings.width - Settings.width / 4;
+        restartButton.y = Settings.height / 3;
+        restartButton.width = Settings.buttonWidth / 2;
+        restartButton.height = Settings.buttonHeight / 2;
+
+        quitButton.x = Settings.width - Settings.width / 4;
+        quitButton.y = Settings.height / 2;
+        quitButton.width = Settings.buttonWidth / 2;
+        quitButton.height = Settings.buttonHeight / 2;
 
 
     }
@@ -30,10 +41,27 @@ public class Game extends Canvas{
     @Override
     public void mouseEvent(MouseEvent mouseEvent) {
 
+        int x, y;
 
+        if (mouseEvent.isLeftMouseButton()) {
+
+            x = mouseEvent.getX();
+            y = mouseEvent.getY();
+
+            if (MyButton.checkBounds(x, y,
+                    quitButton.x, quitButton.y, quitButton.width, quitButton.height)) {
+
+                switchToScreen(new Main());
+
+            }
+        }
     }
 
     private void draw(){
+
+        MyButton.drawButton(restartButton.x, restartButton.y, restartButton.width, restartButton.height, Settings.fontSize / 2, "Restart Game");
+        MyButton.drawButton(quitButton.x, quitButton.y, quitButton.width, quitButton.height, Settings.fontSize / 2, "Back to Menu");
+
         GridDraw gridDraw = new GridDraw();
         gridDraw.drawGrid();
 
