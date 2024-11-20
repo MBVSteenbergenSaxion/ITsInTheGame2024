7f2bafb0ff961a748fb6c9bb1afa44e50420e6ff
MBVSteenbergen <572430@student.saxion.nl>
2024-11-20 19:25:33 +0100

    [Block] Drawn a Tetrisblock on the grid

--- src/Grid/Block.java
+++ src/Grid/Block.java
@@ -1,9 +1,23 @@
 package Grid;
 
+import nl.saxion.app.SaxionApp;
+
+import java.awt.*;
 import java.util.ArrayList;
 
 public class Block {
-    public int x, y, width, height;
-    public boolean filled = false;
-    
+
+    private int[][] block = { {1,0}, {1,0}, {1,1} };
+
+
+    public void drawBlock(Color color, int x, int y) {
+        for (int r = 0; r < block.length; r++) {
+            for (int b = 0; b < block[0].length; b++) {
+                if(block[r][b] == 1) {
+                    SaxionApp.setFill(color);
+                    SaxionApp.drawRectangle(GridSettings.blockSize * b + x, r * GridSettings.blockSize + y, GridSettings.blockSize, GridSettings.blockSize);
+                }
+            }
+        }
+    }
 }
