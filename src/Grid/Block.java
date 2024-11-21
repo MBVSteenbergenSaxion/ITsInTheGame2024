a0bc9230fdf6b5148bf50b7a8edba0cf747994a8
MBVSteenbergen <572430@student.saxion.nl>
2024-11-21 23:27:41 +0100

    [Block] Rotation
    Made it possible to rotate a block. Logic behind it is in the description

--- src/Grid/Block.java
+++ src/Grid/Block.java
@@ -16,13 +16,40 @@ public class Block {
     private int[][] shape;
     private Color color;
     private int x,y;
+    private int[][][] shapes;
+    private int currentRotation;
 
     public Block(int[][] shape, Color color) {
         this.shape = shape;
         this.color = color;
+
+        initShapes();
+    }
+
+    //Controls the shape with the rotations (4 rotations)
+    private void initShapes() {
+        shapes = new int[4][][];
+
+        //Here it becomes rotated (explained in rotation method)
+        for (int i = 0; i < 4; i++) {
+            int r = shape[0].length;
+            int c = shape.length;
+            shapes[i] = new int[r][c];
+
+            for (int y = 0; y < r; y++) {
+                for (int x = 0; x < c; x++) {
+                    shapes[i][y][x] = shape[c - x - 1][y];
+                }
+            }
+
+            shape = shapes[i];
+        }
     }
 
     public void spawn() {
+        currentRotation = 0;
+        shape = shapes[currentRotation];
+
         y = -getHeight();
         x = (GridSettings.width - getWidth()) / 2;
     }
@@ -61,6 +88,17 @@ public class Block {
 
     public void moveRight() {
         x++;
+    }
+
+    //2 sorts of array, array 1 has the same amount of columns (width) as array 2 has rows
+    //AND where array 1 has the same amount of rows (height) as array 2 has columns (You rotate the block 90 degrees)
+    //ALSO the column of array 1 equals to the row of array 2
+    public void rotate() {
+        currentRotation++;
+        if (currentRotation > 3) {
+            currentRotation = 0;
+        }
+        shape = shapes[currentRotation];
     }
 
     public int getBottomEdge() {
