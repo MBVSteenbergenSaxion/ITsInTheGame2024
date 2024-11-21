537ae0ab6f9f87e318493d5c3554dc1ed9189557
MBVSteenbergen <572430@student.saxion.nl>
2024-11-21 22:39:26 +0100

    [Blocks] Spawns block
    After that makes a while leep that uses the thread for moving the block downwards. If it can't move down anymore a new block is spawned

--- src/GameThread.java
+++ src/GameThread.java
@@ -20,13 +20,19 @@ public class GameThread extends Thread {
 
     @Override
     public void run() {
+
         while(true){
-            gridDraw.moveBlockDown();
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                Logger.getLogger(GameThread.class.getName()).log(Level.SEVERE,null, e);
+            gridDraw.spawnBlock();
+
+            while (gridDraw.moveBlockDown()) {
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    Logger.getLogger(GameThread.class.getName()).log(Level.SEVERE,null, e);
+                }
             }
+
         }
 
     }
