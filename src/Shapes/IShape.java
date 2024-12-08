eb6aab57d9921f8ed84d83a556fa5c52d2d7778b
MBVSteenbergen <572430@student.saxion.nl>
2024-12-08 12:22:44 +0100

    [IShape] less commments

--- src/Shapes/IShape.java
+++ src/Shapes/IShape.java
@@ -5,14 +5,13 @@ import java.awt.*;
 
 public class IShape extends Shape {
 
-    //With super the shape object below wil be made as: private int[][] shape in Block.java
     public IShape() {
         super(
-                new Color(255, 0 ,0), // Color
-                4, // Dimension
-                4, // Spawn Column
-                1, // Spawn Row
-                new boolean[][][] { // 3D array for rotation states
+                new Color(255, 0 ,0),
+                4,
+                4,
+                1,
+                new boolean[][][] {
                         {
                                 {false, false, false, false},
                                 {true, true, true, true},
