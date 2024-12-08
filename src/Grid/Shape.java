7624a94eff545eb604b1609b1ddc760a77c5c074
MBVSteenbergen <572430@student.saxion.nl>
2024-12-08 23:48:06 +0100

    [Shape] Returns all the first sides of a block in its dimension

--- src/Grid/Shape.java
+++ src/Grid/Shape.java
@@ -25,7 +25,7 @@ public class Shape {
 
         //en initialiseer de startwaardes met col is de helft van het scherm - (de dimensie van de shape/2)
         SPAWN_COL = (GridSettings.startX_PANEL + ((GridSettings.GRID_COLUMNS * GridSettings.BLOCK_SIZE) / 2)) - (DIMENSION / 2);
-        SPAWN_ROW = getTopSide(0);
+        SPAWN_ROW = getTopSide(3);
     }
 
     public Color getBaseColor() {
@@ -59,11 +59,53 @@ public class Shape {
     public int getRows() {
         return rows;
     }
-    private int getTopSide(int rotation) {
-        return 0;
-    }
 
     public boolean isBlock(int x, int y, int rotation) {
         return blocks[rotation][x][y];
     }
+
+    public int getLeftSideShape(int rotation) {
+        for (int x = 0; x < DIMENSION; x++) {
+            for (int y = 0; y < DIMENSION; y++) {
+                if(isBlock(x, y, rotation)) {
+                    return x;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public int getRightSideShape(int rotation) {
+        for (int x = DIMENSION - 1; x >= 0; x--) {
+            for (int y = 0; y < DIMENSION; y++) {
+                if (isBlock(x,y,rotation)) {
+                    return DIMENSION - x;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public int getTopSide(int rotation) {
+        for (int y = 0; y < DIMENSION; y++) {
+            for (int x = 0; x < DIMENSION; x++) {
+                if(isBlock(x,y,rotation)) {
+                    return y;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public int getBottomSide(int rotation) {
+        for (int y = DIMENSION - 1; y >= 0; y--) {
+            for (int x = 0; x < DIMENSION; x++) {
+                if(isBlock(x,y,rotation)) {
+                    return DIMENSION - y;
+                }
+            }
+        }
+        return -1;
+    }
+
 }
