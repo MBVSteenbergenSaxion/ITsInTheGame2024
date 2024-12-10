51382dab7581a62a05ca16043eeca11bf53a5acd
max-van-oostrom <566973@student.saxion.nl>
2024-12-10 10:58:18 +0100

    [GameBackend] Moved methods/variables from griddraw to gamebackend

--- src/GameBackend.java
+++ src/GameBackend.java
@@ -1,14 +1,45 @@
+import Grid.Block;
+import Shapes.*;
 import nl.saxion.app.SaxionApp;
+
+import java.awt.*;
 
 public class GameBackend {
 
     public static GameThread gt;
     public static GridDraw gd;
 
+    private static Block[] blocks;
+    private static Color[] colors;
+
+    public static int randomBlock, currentBlockId, setRotation, randomColorValue;
+    public static Block nextblock;
+    public static Block currentblock;
+
     public GameBackend() {
 
-        gd = new GridDraw();
+        gd = new GridDraw(this);
         gt = new GameThread(gd);
+
+        blocks = new Block[]{
+                new LShape(),
+                new IShape(),
+                new JShape(),
+                new OShape(),
+                new SShape(),
+                new ZShape(),
+                new TShape()
+        };
+
+        colors = new Color[]{
+                SaxionApp.createColor(4, 83, 255), //Blue
+                SaxionApp.createColor(253, 103, 1), //Orange
+                SaxionApp.createColor(254, 255, 6), //Yellow
+                SaxionApp.createColor(0, 255, 6), //Green
+                SaxionApp.createColor(254, 4, 253), //Pink
+                SaxionApp.createColor(255, 17, 4), //Red
+                SaxionApp.createColor(5, 239, 253) //Cyan
+        };
     }
 
     public void startGame() {
@@ -28,26 +59,121 @@ public class GameBackend {
         gt.interrupt();
         SaxionApp.clear();
         Canvas.stopBackgroundMusic();
-        gd = new GridDraw();
+        gd = new GridDraw(new GameBackend());
         gt = new GameThread(gd);
         gt.start();
         Game.startAudioGame();
     }
 
+
+    public static void spawnBlock() {
+        setCurrentBlock();
+        currentblock.spawn(setRotation);
+        nextblock.nextSpawn();
+    }
+
+    public static void setCurrentBlock() {
+        spawnRotationCheck();
+
+        currentblock = nextblock;
+        currentBlockId = randomBlock;
+
+        setNextPieceCheck();
+    }
+
+    public static void setNextPiece() {
+        randomBlock = SaxionApp.getRandomValueBetween(0, blocks.length);
+        randomColorValue = SaxionApp.getRandomValueBetween(0, colors.length);
+
+        nextblock = blocks[randomBlock];
+        nextblock.color = colors[randomColorValue];
+    }
+
+
+    public static boolean moveBlockDown() {
+        if (!gd.checkBottom()) {
+            return false;
+        }
+
+        currentblock.moveDown();
+        gd.repaint();
+        return true;
+    }
+
     public void rightMovement() {
-        gd.moveBlockRight();
+        if (currentblock == null) {
+            return;
+        }
+
+        if (!gd.checkRight()) {
+            return;
+        }
+
+        currentblock.moveRight();
+        gd.repaint();
     }
 
     public void leftMovement() {
-        gd.moveBlockLeft();
+        if (currentblock == null) {
+            return;
+        }
+
+        if (!gd.checkLeft()) {
+            return;
+        }
+
+        currentblock.moveLeft();
+        gd.repaint();
     }
 
     public void dropBlock() {
-        gd.dropBlock();
+        if (currentblock == null) {
+            return;
+        }
+
+        while (gd.checkBottom()) {
+            currentblock.moveDown();
+        }
+
+        gd.repaint();
     }
 
     public void rotate() {
-        gd.rotateBlock();
+        if (currentblock == null) {
+            return;
+        }
+
+        currentblock.rotate();
+
+        if (currentblock.getLeftEdge() < 0) {
+            currentblock.setX(0);
+        }
+
+        if (currentblock.getRightEdge() >= gd.gridWidth) {
+            currentblock.setX(gd.gridWidth - currentblock.getWidth());
+        }
+
+        if (currentblock.getBottomEdge() >= gd.gridRows) {
+            currentblock.setY(gd.gridRows - currentblock.getHeight());
+        }
+
+        gd.repaint();
+    }
+
+
+    public static void spawnRotationCheck() {
+        if (randomBlock == 1) {
+            setRotation = 1;
+        } else {
+            setRotation = SaxionApp.getRandomValueBetween(0, 4);
+        }
+    }
+
+    public static void setNextPieceCheck() {
+        setNextPiece();
+        if (randomBlock == currentBlockId) {
+            setNextPiece();
+        }
     }
 
     public void checkToPaint() {
